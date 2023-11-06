adf7954b3b07f22872f10b44bfb2951b82890cfb
Emran <emranitmbstu@gmail.com>
2023-11-06 18:53:29 +0600

    comment

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     // bg color and screen size
     <div className="w-full  overflow-x-hidden min-h-screen flex items-center justify-center bg-amber-50">
-      //gallary content
+
       <div className="max-w-7xl mx-auto py-8 ">
         <ShowPhoto />
       </div>
